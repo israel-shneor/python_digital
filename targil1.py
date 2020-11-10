161a2fa6e0b5528274a6be01fda604f79f774ffd
israel-shneor <isrsh01@gmail.com>
2020-11-11 00:27:49 +0200

    aba

---
+++ targil1.py
@@ -0,0 +1,15 @@
+
+
+a=12
+b=11.56
+c="israel"
+
+
+city="ashdod"
+name=input("what is your name?: ")
+mail=input("enter your mail: ")
+print(mail)
+
+print("hello\nhii: " + name + "\nwhatsap\nyou live in israel " + city + "\nyour age is: " + str(28) + "mail: " + mail )
+
+
