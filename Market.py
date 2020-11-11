5fad69f01e3bf8c7ca05495700be1acf8afa8208
israel-shneor <isrsh01@gmail.com>
2020-11-11 10:28:35 +0200

    math

---
+++ Market.py
@@ -0,0 +1,9 @@
+##
+#
+#
+name="israel shneor"
+aga=28
+print(name[::-1])
+print(aga*3)
+
+print ("israel "in" harav Israel vechu")
---
+++ targil2.py
@@ -0,0 +1,13 @@
+####:math * + -  / ** // %
+
+num=int(input("Enter number 4 digit: "))
+
+#alafim
+
+mun=(num//1000)
+print ("Alafim: " + str(mun))
+print("Meot: " + str((num%1000)//100))
+print("Asarot: " + str((num%100)//10))
+print("Ahadot: " + str(num%10))
+
+#jhgg
