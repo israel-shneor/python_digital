54c4faa5a1585525b92d099ded666271361ee88f
israel-shneor <isrsh01@gmail.com>
2020-11-12 11:19:54 +0200

    IP list

---
+++ LAB4.py
@@ -0,0 +1,19 @@
+##שם גיל מייל וטלפון
+#להדפיס למסך
+# ולהוסיךף עוד שתיים ומה האורך של הרשימה רשימה שתי כתבות איפי
+
+name="israel"
+mail="isrsh01@gmail.com"
+age=28
+phone="054-5699065"
+
+listtt=["israel" , 28 , "054-5699065" , "isr@gmail.com"]
+#print("name: " + listtt[0] + "\nAge: " + str(listtt[1]) + "\nPhone: " + listtt[2] + "\nMail: " + listtt[3])
+
+IP=["1.1.1.1","2.2.2.2"]
+
+IP.append("3.3.3.3")
+IP.append("4.4.4.4")
+IP.pop(2)
+print(IP)
+print(str(len(IP)))
